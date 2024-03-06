8e709bda06fca265eec2f6c75b12fb061233b0ce
yscmark <124672723+yscmark@users.noreply.github.com>
2024-03-06 12:36:38 -0800

    add merge and filter tests

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +12,38 @@ class IsMoon implements StringChecker {
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+	@Test(timeout = 500)
+	public void testMergeRightEnd() {
+		List<String> left = Arrays.asList("a", "b", "c");
+		List<String> right = Arrays.asList("a", "d");
+		List<String> merged = ListExamples.merge(left, right);
+		List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+		assertEquals(expected, merged);
+	}
+
+	@Test(timeout = 500)
+	public void testMergeLeftEnd() {
+		List<String> input1 = new ArrayList<>(Arrays.asList("a", "c", "e", "g"));
+		List<String> input2 = new ArrayList<>(Arrays.asList("b", "d", "f"));
+		List<String> merged = ListExamples.merge(input1, input2);
+		List<String> expected = new ArrayList<>(
+			Arrays.asList("a", "b", "c", "d", "e", "f", "g"));
+		assertEquals(expected, merged);
+	}
+
+	@Test 
+	public void testFilter() {
+		IsMoon sc = new IsMoon();
+		List<String> input = new ArrayList<>(Arrays.asList("moon", "sun", "stars"));
+		List<String> expected = new ArrayList<>(Arrays.asList("moon"));
+		assertEquals(expected, ListExamples.filter(input, sc));
+	}
+
+	@Test 
+	public void testFilterSubstring() {
+		IsMoon sc = new IsMoon();
+		List<String> input = new ArrayList<>(Arrays.asList("moon", "sun", "fullmoon"));
+		List<String> expected = new ArrayList<>(Arrays.asList("moon"));
+		assertEquals(expected, ListExamples.filter(input, sc));
+	}
 }
