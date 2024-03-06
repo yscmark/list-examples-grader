dde790ce19f8fbc3e4f0014eb966e81589d99abf
yscmark <124672723+yscmark@users.noreply.github.com>
2024-03-06 12:28:12 -0800

    update grading script

--- grade.sh
+++ grade.sh
@@ -14,3 +14,40 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+	echo "Missing ListExamples.java in student submission"
+	echo "Score: 0/2"
+	exit
+fi
+
+javac -cp $CPATH *.java &> compile.txt
+if [ $? -ne 0 ]
+then 
+	echo "Compilation error"
+	echo "Score: 0/2"
+	exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples &> results.txt
+
+# Score based on methods passed
+if [ $(grep -c "OK" results.txt) -eq 1 ]
+then
+	echo "Score: 2/2"
+	exit
+elif [[ $(grep -c "Merge" results.txt) && $(grep -c "Filter" results.txt) -ge 1 ]] # If both Merge and Filter fail, score is 0
+then
+	echo "Score: 0/2"
+	exit
+else
+	echo "Score: 1/2"
+	exit
+fi
